04d492822fba60500095cb3e3b0e1d4f21c263eb
Vitor Almeida <vitororama15@gmail.com>
2022-09-18 17:53:40 -0300

    save 06

---
+++ L1E7/Carro.java
@@ -0,0 +1,29 @@
+package L1E7;
+
+public class Carro {
+  private String placaCarro;
+  private String marcaCarro;
+  private String modeloCarro;
+  private int anoCarro;
+  private double quilometragem;
+  private double quantidadeCombustivel;
+  private double percuso;
+  
+  
+  public Carro(String placaCarro, String marcaCarro, String modeloCarro, int anoCarro, double quilometragem) {
+    this.placaCarro = placaCarro;
+    this.marcaCarro = marcaCarro;
+    this.modeloCarro = modeloCarro;
+    this.anoCarro = anoCarro;
+    this.quilometragem = quilometragem;
+  }
+
+  public void dadosViagem(double percurso, double quantidadeCombustivel){
+    //considerando que o carro tenha um consumo 15km/litro
+    
+  }
+
+  
+
+  
+}
