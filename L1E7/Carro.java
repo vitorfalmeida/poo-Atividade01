7d47045cb920de846a2fa8a11f09baa703350ff7
Vitor Almeida <vitororama15@gmail.com>
2022-09-19 16:45:25 -0300

    save 07

--- L1E7/Carro.java
+++ L1E7/Carro.java
@@ -7,9 +7,8 @@ public class Carro {
   private int anoCarro;
   private double quilometragem;
   private double quantidadeCombustivel;
-  private double percuso;
-  
-  
+  private double percurso;
+
   public Carro(String placaCarro, String marcaCarro, String modeloCarro, int anoCarro, double quilometragem) {
     this.placaCarro = placaCarro;
     this.marcaCarro = marcaCarro;
@@ -18,12 +17,80 @@ public class Carro {
     this.quilometragem = quilometragem;
   }
 
-  public void dadosViagem(double percurso, double quantidadeCombustivel){
-    //considerando que o carro tenha um consumo 15km/litro
-    
+  public void abastecer(double quantidadeCombustivel) {
+    this.quantidadeCombustivel += quantidadeCombustivel;
+    System.out.println("A quantidade de combustivél que está no tanque é " + getQuantidadeCombustivel() + "L");
   }
 
-  
+  public void percurso(double percurso) {
+    this.percurso += percurso;
+  }
 
-  
+  public void statusDeViagem() {
+    // considerando o consumo do carro de 15km/l
+    double consumo = getpercurso() / 15;
+    if (consumo >= getQuantidadeCombustivel()) {
+      System.out.println("Combustivel insuficiente para trajeto");
+    } else {
+      System.out.println("Durante do trajeto de: " + getpercurso() + "Km" + "\nA quantidade de combustivél gasto foi :"
+          + consumo + "L");
+    }
+  }
+
+  public String getPlacaCarro() {
+    return placaCarro;
+  }
+
+  public void setPlacaCarro(String placaCarro) {
+    this.placaCarro = placaCarro;
+  }
+
+  public String getMarcaCarro() {
+    return marcaCarro;
+  }
+
+  public void setMarcaCarro(String marcaCarro) {
+    this.marcaCarro = marcaCarro;
+  }
+
+  public String getModeloCarro() {
+    return modeloCarro;
+  }
+
+  public void setModeloCarro(String modeloCarro) {
+    this.modeloCarro = modeloCarro;
+  }
+
+  public int getAnoCarro() {
+    return anoCarro;
+  }
+
+  public void setAnoCarro(int anoCarro) {
+    this.anoCarro = anoCarro;
+  }
+
+  public double getQuilometragem() {
+    return quilometragem;
+  }
+
+  public void setQuilometragem(double quilometragem) {
+    this.quilometragem = quilometragem;
+  }
+
+  public double getQuantidadeCombustivel() {
+    return quantidadeCombustivel;
+  }
+
+  public void setQuantidadeCombustivel(double quantidadeCombustivel) {
+    this.quantidadeCombustivel = quantidadeCombustivel;
+  }
+
+  public double getpercurso() {
+    return percurso;
+  }
+
+  public void setpercurso(double percurso) {
+    this.percurso = percurso;
+  }
+
 }
---
+++ L1E7/Main.java
@@ -0,0 +1,16 @@
+package L1E7;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Carro carro01 = new Carro("PQG-1030", "Fiat", "Punto", 2017, 3750);
+
+    
+    carro01.abastecer(11);
+    carro01.percurso(150);
+    carro01.statusDeViagem();
+   
+
+  }
+
+}
